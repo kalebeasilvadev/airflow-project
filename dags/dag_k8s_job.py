838f374d4aa1c369df608b393a193fef0dc742e8
kalebeasilvadev <contato@kalebeasilva.dev>
2025-05-14 11:22:43 -0300

    Add DAG file path logging to KubernetesPodOperator command

--- dags/dag_k8s_job.py
+++ dags/dag_k8s_job.py
@@ -1,6 +1,9 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from datetime import datetime
+import os
+
+dag_file_path = os.path.abspath(__file__)
 
 with DAG(
     dag_id="test_pod_operator",
@@ -9,13 +12,12 @@ with DAG(
     catchup=False,
     tags=["k8s"],
 ) as dag:
-
     test_task = KubernetesPodOperator(
         task_id="run_test_pod",
         name="airflow-test",
         namespace="airflow",
         image="busybox",
-        cmds=["sh", "-c", "echo 'Hello and wait a bit'; sleep 5"],
+        cmds=["sh", "-c", f"echo 'DAG Path: {dag_file_path}'; echo 'Hello and wait a bit'; sleep 5"],
         get_logs=True,
         is_delete_operator_pod=True,  # Deleta após capturar logs
     )
